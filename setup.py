20f5177cae7454455b21cd5f7178f5639c02d385
Sergej Orlov <wladimirych@gmail.com>
2019-10-24 20:36:24 +0200

    create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='maprec',
+    version='1.0.0',
+    url='https://github.com/wladich/maprec',
+    author='Sergey Orlov',
+    author_email='wladimirych@gmail.com',
+    packages=find_packages(),
+)
